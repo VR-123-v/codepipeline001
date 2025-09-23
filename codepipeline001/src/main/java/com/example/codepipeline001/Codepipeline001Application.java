2560f5eba999a3afaadd1a489024f15edf42a01f
VR-123-v <Vrajamohan7974@conestogac.on.ca>
2025-09-23 01:33:23 -0400

    first commit

---
+++ codepipeline001/src/main/java/com/example/codepipeline001/Codepipeline001Application.java
@@ -0,0 +1,13 @@
+package com.example.codepipeline001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codepipeline001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Codepipeline001Application.class, args);
+	}
+
+}
---
+++ codepipeline001/src/test/java/com/example/codepipeline001/Codepipeline001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codepipeline001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codepipeline001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
